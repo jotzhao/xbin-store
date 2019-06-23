4dac7c8158f8397952ea07416bb14a0a6c6957ca
jot.zhao <jot.zhao@gmail.com>
2019-06-23 08:53:47 +0800

    可以编译了

---
+++ xbin-store/xbin-store-web-admin/src/main/java/cn/binux/XbinStoreWebAdminApplication.java
@@ -0,0 +1,18 @@
+package cn.binux;
+
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Configuration;
+
+
+@Configuration
+
+@SpringBootApplication
+//@EnableWebMvc
+public class XbinStoreWebAdminApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(XbinStoreWebAdminApplication.class, args);
+	}
+}
